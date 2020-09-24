2ab99f9b30ce38ea9f10e1f9b1c323984ed781bf
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2020-09-24 12:43:41 +0600

    add custom marker in template

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useLayoutEffect } from "react"
 import "./App.css"
 import * as am4core from "@amcharts/amcharts4/core"
 import * as am4maps from "@amcharts/amcharts4/maps"
-import am4themes_animated from "@amcharts/amcharts4/themes/animated"
 import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow"
-am4core.useTheme(am4themes_animated)
 
 function App(props) {
   useLayoutEffect(() => {
@@ -27,21 +25,29 @@ function App(props) {
     polygonSeries.mapPolygons.template.nonScalingStroke = true
     polygonSeries.exclude = ["AQ"]
 
-    // Add line bullets
+    // Add markers
     var cities = chart.series.push(new am4maps.MapImageSeries())
-    cities.mapImages.template.nonScaling = true
 
-    var city = cities.mapImages.template.createChild(am4core.Circle)
-    city.radius = 6
-    city.fill = chart.colors.getIndex(0).brighten(-0.2)
-    city.strokeWidth = 2
-    city.stroke = am4core.color("#fff")
+    var imageSeriesTemplate = cities.mapImages.template
+    var city = imageSeriesTemplate.createChild(am4core.Image)
+    city.href = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-160/marker.svg"
+    city.width = 20
+    city.height = 20
+    city.nonScaling = true
+    city.horizontalCenter = "middle"
+    city.verticalCenter = "bottom"
+    city.propertyFields.href = "icon"
+
+    // Set property fields
+    imageSeriesTemplate.propertyFields.latitude = "latitude"
+    imageSeriesTemplate.propertyFields.longitude = "longitude"
 
     function addCity(coords, title) {
-      var city = cities.mapImages.create()
+      const city = cities.mapImages.create()
       city.latitude = coords.latitude
       city.longitude = coords.longitude
       city.tooltipText = title
+
       return city
     }
 
@@ -59,19 +65,10 @@ function App(props) {
     lineSeries.mapLines.template.line.strokeDasharray = "1,1"
     lineSeries.zIndex = 10
 
-    var shadowLineSeries = chart.series.push(new am4maps.MapLineSeries())
-    shadowLineSeries.mapLines.template.line.strokeOpacity = 0
-    shadowLineSeries.mapLines.template.line.nonScalingStroke = true
-    shadowLineSeries.mapLines.template.shortestDistance = false
-    shadowLineSeries.zIndex = 5
-
     function addLine(from, to) {
       var line = lineSeries.mapLines.create()
       line.imagesToConnect = [from, to]
       line.line.controlPointDistance = -0.3
-
-      var shadowLine = shadowLineSeries.mapLines.create()
-      shadowLine.imagesToConnect = [from, to]
 
       return line
     }
@@ -98,105 +95,7 @@ function App(props) {
       "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47"
     planeImage.fill = chart.colors.getIndex(2).brighten(-0.2)
     planeImage.strokeOpacity = 0
-
-    var shadowPlane = shadowLineSeries.mapLines.getIndex(0).lineObjects.create()
-    shadowPlane.position = 0
-    shadowPlane.width = 48
-    shadowPlane.height = 48
-
-    var shadowPlaneImage = shadowPlane.createChild(am4core.Sprite)
-    shadowPlaneImage.scale = 0.05
-    shadowPlaneImage.horizontalCenter = "middle"
-    shadowPlaneImage.verticalCenter = "middle"
-    shadowPlaneImage.path =
-      "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47"
-    shadowPlaneImage.fill = am4core.color("#000")
-    shadowPlaneImage.strokeOpacity = 0
-
-    shadowPlane.adapter.add("scale", function (scale, target) {
-      target.opacity = 0.6 - Math.abs(0.5 - target.position)
-      return 0.5 - 0.3 * (1 - Math.abs(0.5 - target.position))
-    })
-
-    // Plane animation
-    var currentLine = 0
-    var direction = 1
-    function flyPlane() {
-      // Get current line to attach plane to
-      plane.mapLine = lineSeries.mapLines.getIndex(currentLine)
-      plane.parent = lineSeries
-      shadowPlane.mapLine = shadowLineSeries.mapLines.getIndex(currentLine)
-      shadowPlane.parent = shadowLineSeries
-      shadowPlaneImage.rotation = planeImage.rotation
-
-      // Set up animation
-      var from, to
-      var numLines = lineSeries.mapLines.length
-      if (direction === 1) {
-        from = 0
-        to = 1
-        if (planeImage.rotation !== 0) {
-          planeImage
-            .animate({ to: 0, property: "rotation" }, 1000)
-            .events.on("animationended", flyPlane)
-          return
-        }
-      } else {
-        from = 1
-        to = 0
-        if (planeImage.rotation !== 180) {
-          planeImage
-            .animate({ to: 180, property: "rotation" }, 1000)
-            .events.on("animationended", flyPlane)
-          return
-        }
-      }
-
-      // Start the animation
-      var animation = plane.animate(
-        {
-          from: from,
-          to: to,
-          property: "position",
-        },
-        5000,
-        am4core.ease.sinInOut
-      )
-      animation.events.on("animationended", flyPlane)
-      /*animation.events.on("animationprogress", function(ev) {
-      var progress = Math.abs(ev.progress - 0.5);
-      //console.log(progress);
-      //planeImage.scale += 0.2;
-    });*/
-
-      shadowPlane.animate(
-        {
-          from: from,
-          to: to,
-          property: "position",
-        },
-        5000,
-        am4core.ease.sinInOut
-      )
-
-      // Increment line, or reverse the direction
-      currentLine += direction
-      if (currentLine < 0) {
-        currentLine = 0
-        direction = 1
-      } else if (currentLine + 1 > numLines) {
-        currentLine = numLines - 1
-        direction = -1
-      }
-    }
-
-    // Go!
-    flyPlane()
-
-    return () => {
-      chart.dispose()
-    }
-  }, [])
+  })
 
   return <div id="chartdiv"></div>
 }
